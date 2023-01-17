5b6da4d4a49da949ffad7f5d307f2fb0495aeb2f
Mihail Antonov <mihail.d.antonov@gmail.com>
2023-01-17 22:52:43 +0200

    Change of document title after 3 secs delay logic implementation

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,8 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
 
-  const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
-  });
+  setTimeout(function () {
+    document.title = 'One new message';
+  }, 3000);
 });
